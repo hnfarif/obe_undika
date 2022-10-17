f0d813f285388031aab1cf97bcef4ffecadbb859
Hanif Arif <hnfarif18@gmail.com>
2022-10-17 12:08:54 +0700

    debug data

--- app/Http/Controllers/InstrumenNilaiController.php
+++ app/Http/Controllers/InstrumenNilaiController.php
@@ -140,6 +140,7 @@ class InstrumenNilaiController extends Controller
             $week = '16';
         }
 
+        dd($week);
         $rps = Rps::where('id', $instru->rps_id)->first();
 
         $getPekan = AgendaBelajar::where('rps_id', $rps->id)->where('pekan', $week)->first();
