633a7413b00c4924ea8d015eda0966d31faded2b
Hanif Arif <hnfarif18@gmail.com>
2022-10-17 15:30:18 +0700

    fix export excel

--- app/Exports/MonevExport.php
+++ app/Exports/MonevExport.php
@@ -4,6 +4,8 @@ namespace App\Exports;
 
 use App\Models\JadwalKuliah;
 use App\Models\KriteriaMonev;
+use App\Models\PlottingMonev;
+use App\Models\Semester;
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
 
@@ -11,9 +13,12 @@ class MonevExport implements FromView
 {
     public function view(): View
     {
+        $smt = Semester::orderBy('smt_yad', 'desc')->first();
+        $plot = PlottingMonev::whereSemester($smt->smt_yad)->pluck('klkl_id')->toArray();
+
         return view('laporan.monev.export-excel', [
             'kri' => KriteriaMonev::orderBy('id', 'asc')->get(),
-            'jdw' => JadwalKuliah::with('matakuliahs', 'karyawans')->fakultas()->prodi()->dosen()->get(),
+            'jdw' => JadwalKuliah::whereIn('klkl_id', $plot)->with('matakuliahs', 'karyawans')->fakultas()->prodi()->dosen()->get(),
         ]);
     }
 }
