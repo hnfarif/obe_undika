ca12e75a27457d212ebc40380a66d838e37e87af
Hanif Arif <hnfarif18@gmail.com>
2022-10-17 10:24:24 +0700

    debug data

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -29,7 +29,7 @@ class ApiController extends Controller
     }
 
     public function cekData(){
-        $mk = JadwalKuliah::all();
+        $mk = krs::all();
 
         //json
         $data = json_encode($mk);
