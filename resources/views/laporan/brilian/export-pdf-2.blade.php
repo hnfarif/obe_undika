5cf6bb58ed770b44fcf703a6d9d31e4c750b80fe
Hanif Arif <hnfarif18@gmail.com>
2022-10-18 14:49:35 +0700

    fix export pdf view

--- resources/views/laporan/brilian/export-pdf-2.blade.php
+++ resources/views/laporan/brilian/export-pdf-2.blade.php
@@ -85,7 +85,7 @@
                 </tr>
                 <tr>
                     @foreach ($indikator as $i)
-                    <th>{{ $i['nama'] }}</th>
+                    <th>{{ $i->nama }}</th>
                     <th>Skor</th>
                     @endforeach
                 </tr>
@@ -159,7 +159,7 @@
                 </tr>
                 <tr>
                     @foreach ($indikator as $i)
-                    <th>{{ $i['nama'] }}</th>
+                    <th>{{ $i->nama }}</th>
                     <th>Skor</th>
                     @endforeach
                 </tr>
