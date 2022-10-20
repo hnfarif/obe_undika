4ce329603e4e00a447b7ae556da720de9f8cf1ae
Hanif Arif <hnfarif18@gmail.com>
2022-10-20 15:03:11 +0700

    debug data

--- app/Http/Controllers/LaporanAngketController.php
+++ app/Http/Controllers/LaporanAngketController.php
@@ -40,7 +40,6 @@ class LaporanAngketController extends Controller
         foreach ($plot as $key =>  $p) {
             $data[$p->nik_pengajar]['nama'] = $p->karyawan->nama;
             $data[$p->nik_pengajar]['rata_dosen'] = $angket->where('nik', $p->nik_pengajar)->avg('nilai');
-            $data[$p->nik_pengajar]['matakuliah'][$p->klkl_id] = $p->klkl_id;
             $data[$p->nik_pengajar]['matakuliah'][$p->klkl_id]['nama'] = $p->matakuliah->nama;
             $data[$p->nik_pengajar]['matakuliah'][$p->klkl_id]['kelas'] = $p->kelas;
             $data[$p->nik_pengajar]['matakuliah'][$p->klkl_id]['rata_mk'] = $angket->where('nik', $p->nik_pengajar)->where('kode_mk', $p->klkl_id)->where('kelas', $p->kelas)->avg('nilai');
