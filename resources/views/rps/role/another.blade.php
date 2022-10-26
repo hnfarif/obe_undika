fc54a887972951033adfea89b1868196dc9e5d8a
Hanif Arif <hnfarif18@gmail.com>
2022-10-26 13:31:17 +0700

    delete data done

--- resources/views/rps/role/another.blade.php
+++ resources/views/rps/role/another.blade.php
@@ -1,8 +1,3 @@
-<form action="{{ route('cekData') }}" method="post">
-    @csrf
-    @method('DELETE')
-    <button type="submit" class="btn btn-danger">Hapus</button>
-</form>
 @if (session()->has('message'))
 <div class="alert {{ session()->get('alert-class') }} alert-dismissible fade show" role="alert">
     {{ session()->get('message') }}
