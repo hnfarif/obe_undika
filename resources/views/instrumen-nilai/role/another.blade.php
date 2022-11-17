dfcbf25c8347a6fe053f111ea8b0cbacd8c95453
Hanif Arif <hnfarif18@gmail.com>
2022-11-17 21:30:58 +0700

    maintenance grafik

--- resources/views/instrumen-nilai/role/another.blade.php
+++ resources/views/instrumen-nilai/role/another.blade.php
@@ -81,13 +81,13 @@
 </div>
 
 <div class="row rangCapaiClo d-none">
-    <div class="col-12 col-lg-12 col-md-6" onload="grafikCapaiClo();">
+    <div class="col-12 col-lg-12 col-md-6">
         <div class="card">
             <div class="card-header">
                 <h4 class="card-title">Grafik Ketercapaian CLO</h4>
             </div>
             <div class="card-body">
-                <div>
+                <div onload="grafikCapaiClo();">
 
                     <canvas id="grangclo" width="700" height="700"></canvas>
                 </div>
