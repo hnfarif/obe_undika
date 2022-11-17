db14a320950a983781709f63b51250702f1283b1
Hanif Arif <hnfarif18@gmail.com>
2022-11-17 20:39:27 +0700

    maintenance grafik

--- resources/views/instrumen-nilai/script.blade.php
+++ resources/views/instrumen-nilai/script.blade.php
@@ -2,11 +2,55 @@
 <script>
     $("#tableJdw").DataTable();
 
-    function createGrafik() {
+
+
+    $(document).ready(function () {
+
+        $(".btnUbahNilai").on('click', function () {
+
+            $.ajax({
+                url: "{{  route('penilaian.cekrps') }}",
+                type: 'GET',
+                dataType: 'json',
+                data: {
+                    'kode_mk': $(this).data('mk'),
+                    'nik': $(this).data('nik'),
+                    'kelas': $(this).data('kelas'),
+                },
+                success: function (data) {
+                    if ($.isEmptyObject(data.error)) {
+                        window.location.href = data.url;
+                    } else {
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Oops, Ada yang salah!',
+                            text: data.error,
+                        })
+
+                    }
+                }
+
+            })
+        })
+
+        $('input[type=radio][name=optrangclo]').change(function () {
+            if ($(this).val() == 'dafIns') {
+                $('.dafIns').removeClass('d-none');
+                $('.rangCapaiClo').addClass('d-none');
+            } else if ($(this).val() == 'rangCapaiClo') {
+                $('.dafIns').addClass('d-none');
+                $('.rangCapaiClo').removeClass('d-none');
+
+            }
+        })
+
         $.ajax({
             url: "{{ route('penilaian.rangkumCapaiClo') }}",
             type: 'GET',
             dataType: 'json',
+            data: {
+                'smt': "{{ $smt }}",
+            },
             beforeSend: function () {
                 $('.rangCapaiClo').html(
                     '<div class="text-center"><i class="fa fa-spinner fa-spin fa-3x fa-fw"></i> Harap tunggu sebentar ....</div>'
@@ -57,47 +101,6 @@
             },
 
 
-        })
-    }
-
-    $('input[type=radio][name=optrangclo]').change(function () {
-        if ($(this).val() == 'dafIns') {
-            $('.dafIns').removeClass('d-none');
-            $('.rangCapaiClo').addClass('d-none');
-        } else if ($(this).val() == 'rangCapaiClo') {
-            $('.dafIns').addClass('d-none');
-            $('.rangCapaiClo').removeClass('d-none');
-
-        }
-    })
-
-    $(document).ready(function () {
-
-        $(".btnUbahNilai").on('click', function () {
-
-            $.ajax({
-                url: "{{  route('penilaian.cekrps') }}",
-                type: 'GET',
-                dataType: 'json',
-                data: {
-                    'kode_mk': $(this).data('mk'),
-                    'nik': $(this).data('nik'),
-                    'kelas': $(this).data('kelas'),
-                },
-                success: function (data) {
-                    if ($.isEmptyObject(data.error)) {
-                        window.location.href = data.url;
-                    } else {
-                        Swal.fire({
-                            icon: 'error',
-                            title: 'Oops, Ada yang salah!',
-                            text: data.error,
-                        })
-
-                    }
-                }
-
-            })
         })
 
     })
