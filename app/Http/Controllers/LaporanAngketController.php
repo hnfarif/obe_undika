a186ae8123f88345ce3130f7251384158f431935
Hanif Arif <hnfarif18@gmail.com>
2022-11-17 11:36:33 +0700

    debug agenda

--- app/Http/Controllers/LaporanAngketController.php
+++ app/Http/Controllers/LaporanAngketController.php
@@ -61,13 +61,15 @@ class LaporanAngketController extends Controller
             $data[$p->nik_pengajar]['matakuliah'][$p->klkl_id][$p->kelas]['rata_mk'] = number_format($rata_mk, 2);
 
             $count = count($data[$p->nik_pengajar]['matakuliah']);
-            $sum = array_sum(array_column($data[$p->nik_pengajar]['matakuliah'], 'rata_mk'));
+            $sum = 0;
             // average per nik_pengajar
 
             // dd($count);
-            // foreach ($data[$p->nik_pengajar]['matakuliah'] as $mk) {
-            //     $sum += $mk[$p->kelas]['rata_mk'];
-            // }
+            foreach ($data[$p->nik_pengajar]['matakuliah'] as $mk) {
+                foreach ($mk as $kls) {
+                    $sum += $kls['rata_mk'];
+                }
+            }
 
             $avgDosen = number_format($sum / $count, 2);
             $data[$p->nik_pengajar]['rata_dosen'] = $sum;
