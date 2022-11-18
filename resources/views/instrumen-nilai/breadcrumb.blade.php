17d659a271eeef9f71b72a3d5115f027e1fd5e24
Hanif Arif <hnfarif18@gmail.com>
2022-11-18 19:04:29 +0700

    fix breadcrumb

--- resources/views/instrumen-nilai/breadcrumb.blade.php
+++ resources/views/instrumen-nilai/breadcrumb.blade.php
@@ -2,14 +2,14 @@
     <ol class="breadcrumb">
         <?php $link = "" ?>
         @for($i = 1; $i <= count(Request::segments()); $i++) @if($i < count(Request::segments()) & $i> 0)
-            <?php $link .= "/" . Request::segment($i); ?>
+            <?php $link .= "/public/" . Request::segment($i); ?>
 
             <li class="breadcrumb-item">
                 <a href="<?= $link ?>">{{ucwords(str_replace('-',' ',Request::segment($i)))}}</a>
             </li>
             @else
             <li class="breadcrumb-item active" aria-current="page">
-                <a href="<?= $link ?>">{{ucwords(str_replace('-',' ',Request::segment($i)))}}</a>
+                {{ucwords(str_replace('-',' ',Request::segment($i)))}}
             </li>
             @endif
             @endfor
