60c5717156573f71834a61e73f19155d86d32c6a
Hanif Arif <hnfarif18@gmail.com>
2022-11-18 10:42:01 +0700

    move ajax

--- resources/views/instrumen-nilai/script.blade.php
+++ resources/views/instrumen-nilai/script.blade.php
@@ -38,60 +38,62 @@
             } else if ($(this).val() == 'rangCapaiClo') {
                 $('.dafIns').addClass('d-none');
                 $('.rangCapaiClo').removeClass('d-none');
-                var ctx = document.getElementById('grangclo').getContext('2d');
-                $.ajax({
-                    url: "{{ route('penilaian.rangkumCapaiClo') }}",
-                    type: 'GET',
-                    dataType: 'json',
-                    beforeSend: function () {
-                        $('.loadGrafik').html(
-                            '<div class="text-center"><i class="fa fa-spinner fa-spin fa-3x fa-fw"></i> Harap tunggu sebentar ....</div>'
-                        );
-                    },
-                    success: function (data) {
-                        if ($.isEmptyObject(data.error)) {
-                            $('.loadGrafik').html('');
-                            const myChart = new Chart(ctx, {
-                                type: 'bar',
-                                data: {
-                                    labels: ["Total MK tercapai",
-                                        "Total MK belum tercapai"
-                                    ],
-                                    datasets: [{
-                                        data: [data.jmlInsLulus, data
-                                            .jmlInsTdkLulus
-                                        ],
-                                        backgroundColor: [
-                                            'rgba(255, 99, 132, 0.2)',
-                                            'rgba(54, 162, 235, 0.2)',
-                                        ],
-                                        borderColor: [
-                                            'rgba(255, 99, 132, 1)',
-                                            'rgba(54, 162, 235, 1)',
-                                        ],
-                                        borderWidth: 1
-                                    }]
-                                },
-                                options: {
-                                    scales: {
-                                        y: {
-                                            beginAtZero: true
-                                        }
 
-                                    }
-                                }
-                            });
-                        } else {
-                            Swal.fire({
-                                icon: 'error',
-                                title: 'Oops, Ada yang salah!',
-                                text: 'Terjadi kesalahan saat mengambil data',
-                            })
-
-                        }
-                    },
-                })
             }
+        })
+
+        var ctx = document.getElementById('grangclo').getContext('2d');
+        $.ajax({
+            url: "{{ route('penilaian.rangkumCapaiClo') }}",
+            type: 'GET',
+            dataType: 'json',
+            beforeSend: function () {
+                $('.loadGrafik').html(
+                    '<div class="text-center"><i class="fa fa-spinner fa-spin fa-3x fa-fw"></i> Harap tunggu sebentar ....</div>'
+                );
+            },
+            success: function (data) {
+                if ($.isEmptyObject(data.error)) {
+                    $('.loadGrafik').html('');
+                    const myChart = new Chart(ctx, {
+                        type: 'bar',
+                        data: {
+                            labels: ["Total MK tercapai",
+                                "Total MK belum tercapai"
+                            ],
+                            datasets: [{
+                                data: [data.jmlInsLulus, data
+                                    .jmlInsTdkLulus
+                                ],
+                                backgroundColor: [
+                                    'rgba(255, 99, 132, 0.2)',
+                                    'rgba(54, 162, 235, 0.2)',
+                                ],
+                                borderColor: [
+                                    'rgba(255, 99, 132, 1)',
+                                    'rgba(54, 162, 235, 1)',
+                                ],
+                                borderWidth: 1
+                            }]
+                        },
+                        options: {
+                            scales: {
+                                y: {
+                                    beginAtZero: true
+                                }
+
+                            }
+                        }
+                    });
+                } else {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops, Ada yang salah!',
+                        text: 'Terjadi kesalahan saat mengambil data',
+                    })
+
+                }
+            },
         })
 
     });
