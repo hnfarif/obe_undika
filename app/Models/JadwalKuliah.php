f11a95053a0f680b7342d201676abf01b336453a
Hanif Arif <hnfarif18@gmail.com>
2022-11-22 22:07:57 +0700

    maintenance detail dosen

--- app/Models/JadwalKuliah.php
+++ app/Models/JadwalKuliah.php
@@ -259,7 +259,7 @@ class JadwalKuliah extends Model
 
     public function getKelulusanMk($mk, $nik)
     {
-        $mkFilter = $mk->where('kary_nik', $nik)->all();
+        $mkFilter = $mk->where('kary_nik', $nik);
         return $mkFilter;
     }
 }
