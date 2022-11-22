9060c0768798cddfc01d2a48a563e4acca1bfec7
Hanif Arif <hnfarif18@gmail.com>
2022-11-22 21:21:19 +0700

    maintenance detail dosen

--- resources/views/instrumen-nilai/capai-clo-list.blade.php
+++ resources/views/instrumen-nilai/capai-clo-list.blade.php
@@ -64,7 +64,7 @@
                                                 </td>
 
                                                 <td>
-                                                    <a href="{{ route('penilaian.detailInstrumen', ['nik' => $k->kary_nik, 'mkTdkLulus' => $mkTdkLulus->where('kary_nik', $k->kary_nik)]) }}"
+                                                    <a href="{{ route('penilaian.detailInstrumen', ['mkTdkLulus' => $mkTdkLulus->where('kary_nik', $k->kary_nik)]) }}"
                                                         class="btn btn-primary btn-sm text-sm">Detail</a>
                                                 </td>
                                             </tr>
