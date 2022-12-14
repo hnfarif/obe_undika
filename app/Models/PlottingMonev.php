4da0352f25cbf1671b1dee71444c69ddc524d5f2
Hanif Arif <hnfarif18@gmail.com>
2022-12-14 23:18:27 +0700

    fix detail plotting v5

--- app/Models/PlottingMonev.php
+++ app/Models/PlottingMonev.php
@@ -45,7 +45,7 @@ class PlottingMonev extends Model
 
     public function getKelasRuang($mk, $nik, $kelas)
     {
-        $maku = JadwalKuliah::where('klkl_id', $mk)->where('nik_pengajar', $nik)->where('kelas', $kelas)->first();
+        $maku = JadwalKuliah::where('klkl_id', $mk)->where('kary_nik', $nik)->where('kelas', $kelas)->first();
 
         return $maku->ruang_id;
     }
