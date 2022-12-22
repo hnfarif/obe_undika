ccc2584182dc5e11a79ff220ab0c69548fa05059
Hanif Arif <hnfarif18@gmail.com>
2022-12-23 02:00:32 +0700

    fix button lihat data

--- resources/views/instrumen-nilai/role/another.blade.php
+++ resources/views/instrumen-nilai/role/another.blade.php
@@ -6,13 +6,16 @@
         <div class="card">
             <div class="card-header">
                 <h4 class="card-title">Grafik Ketercapaian CLO Mata Kuliah</h4>
-                @if (auth()->user()->role != 'kaprodi')
-                <button class="btn btn-primary ml-auto mr-2" data-toggle="modal" data-target="#filInsClo">
-                    <i class="fas fa-filter"></i> Filter
-                </button>
-                @endif
+                <div class="d-flex ml-auto">
+                    @if (auth()->user()->role != 'kaprodi')
+                    <button class="btn btn-primary mr-2" data-toggle="modal" data-target="#filInsClo">
+                        <i class="fas fa-filter"></i> Filter
+                    </button>
+                    @endif
 
-                <a href="{{ route('penilaian.rangkumCapaiCloList') }}" class="btn btn-info ml-auto"> Lihat Data </a>
+                    <a href="{{ route('penilaian.rangkumCapaiCloList') }}" class="btn btn-info"> Lihat Data </a>
+                </div>
+
             </div>
             <div class="card-body">
                 <canvas id="grangclo" width="700" height="200"></canvas>
