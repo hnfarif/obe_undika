0d6e11de09baf40a802144cb26f5f06c70ced3b0
Hanif Arif <hnfarif18@gmail.com>
2022-12-23 13:58:50 +0700

    add session alert detail instrumen clo

--- resources/views/instrumen-nilai/detail.blade.php
+++ resources/views/instrumen-nilai/detail.blade.php
@@ -9,6 +9,15 @@
 
             <div class="section-body">
 
+                @if (session()->has('message'))
+                <div class="alert {{ session()->get('alert-class') }} alert-dismissible fade show" role="alert">
+                    {{ session()->get('message') }}
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                @endif
+
                 <div class="my-3 d-flex">
                     <h5>{{ 'Daftar Instrumen Penilaian CLO - '.$kary->nama }}</h5>
                     <form class="card-header-form ml-auto" action="{{ route('penilaian.clo.index') }}">
