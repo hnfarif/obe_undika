5c2c981bee2d0f5d9b561a34e11d6dc2cde76f99
Hanif Arif <hnfarif18@gmail.com>
2023-01-05 18:03:31 +0700

    fix query karyawan on create

--- app/Http/Controllers/RpsController.php
+++ app/Http/Controllers/RpsController.php
@@ -101,7 +101,7 @@ class RpsController extends Controller
 
         }
         $mk = $filMk;
-        $dosens = KaryawanDosen::where('fakul_id', $prodi->id)->where('kary_type', 'like', '%TD%')->get();
+        $dosens = KaryawanDosen::where('fakul_id', $prodi->id)->where('kary_type', 'like', '%D%')->get();
 
         return view('rps.create', compact('mk','dosens', 'smt'));
     }
