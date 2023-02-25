91c2800fcfa576a9c0803b089a68a9c27163a067
Hanif Arif <hnfarif18@gmail.com>
2023-02-25 18:55:14 +0700

    debug data

--- app/Http/Controllers/LaporanAngketController.php
+++ app/Http/Controllers/LaporanAngketController.php
@@ -23,18 +23,8 @@ class LaporanAngketController extends Controller
 
     public function index()
     {
-        // $fak = Fakultas::where('sts_aktif', 'Y')->get();
-        // $arrFak = $fak->pluck('id')->toArray();
-        // $prodi = Prodi::whereIn('id_fakultas', $arrFak)->where('sts_aktif', 'Y')->get();
-        // $kary = KaryawanDosen::where('kary_type', 'like', '%D%')->get();
 
         $angket = AngketTrans::where('smt', '221')->with('karyawan')->get()->groupBy('nik');
-
-        // $angket = $this->manipulateDataAngket($prodi, $fak)['data'];
-
-        // $rataProdi = [$this->manipulateDataAngket($prodi, $fak)['rataProdi'];
-
-        // $rataFak = $this->manipulateDataAngket($prodi, $fak)['rataFakultas'];
 
         $smt = $this->semester;
 
