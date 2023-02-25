6135ae91779179e6ea803d53831251d14c4b449f
Hanif Arif <hnfarif18@gmail.com>
2023-02-25 18:08:37 +0700

    debug data

--- app/Http/Controllers/LaporanAngketController.php
+++ app/Http/Controllers/LaporanAngketController.php
@@ -123,7 +123,9 @@ class LaporanAngketController extends Controller
     public function cekData()
     {
 
-        $clo = AngketTrans::groupBy('nik')->selectRaw('nik, kode_mk, kelas, avg(nilai) as rata_dosen')->where('smt', '221')->with('karyawan')->get();
+        $clo = AngketTrans::groupBy('nik','kode_mk', 'kelas')->selectRaw('nik, kode_mk, kelas, avg(nilai) as rata_dosen')->where('smt', '221')->get();
+
+        dd($clo);
 
         return [
             'clo' => $clo,
