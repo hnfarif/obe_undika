6daf1cf798975fba1bfc95aee837064b919c98ae
Hanif Arif <hnfarif18@gmail.com>
2023-03-20 16:31:03 +0700

    debug instrumen monev

--- app/Http/Controllers/InstrumenMonevController.php
+++ app/Http/Controllers/InstrumenMonevController.php
@@ -69,7 +69,7 @@ class InstrumenMonevController extends Controller
             $dtlAgd = DetailAgenda::whereIn('agd_id', $agd->pluck('id')->toArray())->with('penilaian','clo','detailInstrumenNilai','agendaBelajar', 'detailInstrumenMonev')->orderby('clo_id', 'asc')->orderby('id', 'asc')->get();
             $dtlInsMon = $cekInsMon->detailMonev;
 
-            $kul = MingguKuliah::where('jenis_smt', 'T')->where('smt', $this->semester)->get();
+            $kul = MingguKuliah::where('jenis_smt', 'T')->where('smt', '221')->get();
 
             $weekEigth = [];
             $weekSixteen = [];
