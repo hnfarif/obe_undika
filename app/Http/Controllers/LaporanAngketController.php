901d3af30cf802e244d09dabd807e8106a10c209
Hanif Arif <hnfarif18@gmail.com>
2023-03-22 08:24:19 +0700

    debug laporan angket

--- app/Http/Controllers/LaporanAngketController.php
+++ app/Http/Controllers/LaporanAngketController.php
@@ -88,7 +88,6 @@ class LaporanAngketController extends Controller
         $fak = $this->fakultas;
 
         $angket = $this->angket;
-        dd($angket);
         $rata = $this->manipulateDataAngket($angket, $fak);
 
         $pdf = PDF::loadView('laporan.angket.export-pdf', ['fak' => $fak,
